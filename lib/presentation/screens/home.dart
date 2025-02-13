af06a8a72e87838d01ac8a1d3a49886dd852e1d7
Aruuuuu <armandobagasonjr28@gmail.com>
2025-02-13 13:21:04 +0800

    Update home.dart

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -14,8 +14,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final List<String> timeSlots = [
-    '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM',
-    '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM'
+    '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '12:30PM', 
+    '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '5:30PM', '6:00PM',
   ];
 
   late DateTime selectedDate;
